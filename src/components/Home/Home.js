6c305a980ab0dfc7d8b05b3ef77c46e8bb8be87f
ZahraO1 <zahra.masroor6@gmail.com>
2022-07-27 16:48:27 -0400

    changed link to format json

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,9 +9,9 @@ function Home() {
   let [data, setData] = useState([])
 
   useEffect(() => {
-        const SONG_API = `http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=a6abe65cf5f35881c07d4928ea5a53be`
+        const API_URL = `http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=a6abe65cf5f35881c07d4928ea5a53be&format=json`
       async function SongList(){
-          const response = await fetch(SONG_API)
+          const response = await fetch(API_URL)
           const resData = await response.json()
         //   console.log(resData.tracks.track)
           setData(resData.tracks.track)
